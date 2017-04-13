10bd8d0694d6dea11cbaa5db0f30f0b6f24d5b60
120764482 <120764482@qq.com>
2017-04-13 19:51:20 +0800

    4.13 w

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -1,1 +1,10 @@
-$('.carousel').carousel()
+$('.carousel').carousel();
+$("#menu").click(function() {
+	if($(".column-right").css("display") == "none") {
+		$(".column-right").show();
+		$(".row").css("height","87px")
+	} else {
+		$(".column-right").hide();
+		$(".row").css("height","48px")
+	}
+}); 
