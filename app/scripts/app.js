57c739d675cff8b35acbe6d88cae77df3d4e2066
120764482 <120764482@qq.com>
2017-04-13 11:55:39 +0800

    4.13

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name guaguaxicheApp
+ * @description
+ * # guaguaxicheApp
+ *
+ * Main module of the application.
+ */
+//angular
+//.module('guaguaxicheApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guaguaxicheApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the guaguaxicheApp
+ */
+//angular.module('guaguaxicheApp')
+//.controller('MainCtrl', function () {
+//  this.awesomeThings = [
+//    'HTML5 Boilerplate',
+//    'AngularJS',
+//    'Karma'
+//  ];
+//});
